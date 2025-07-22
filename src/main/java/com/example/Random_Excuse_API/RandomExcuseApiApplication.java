821f1f397310f4488cf0ca56b73eab0af2a3ec36
Mikołaj Polecki <miki.polecki@gmail.com>
2025-07-22 20:49:17 +0200

    Initialize Spring Boot
    Set up a new Spring Boot project with Gradle build configuration, wrapper scripts, and basic application structure. Includes main application class, test class, and initial application properties.

---
+++ src/main/java/com/example/Random_Excuse_API/RandomExcuseApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Random_Excuse_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomExcuseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomExcuseApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Random_Excuse_API/RandomExcuseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Random_Excuse_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomExcuseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
